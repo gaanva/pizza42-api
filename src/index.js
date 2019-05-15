3bd2b62805b7f7a53195c47985ffa52ca0956c34
Antornio <gaanva@gmail.com>
2019-05-15 15:06:06 -0300

    Order services finished

--- src/index.js
+++ src/index.js
@@ -1,64 +1,62 @@
 // importing the dependencies
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+// defining the Express app
+const app = express();
 //required for token and key set.
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
-
 //adding authorization
 const jwtAuthz = require('express-jwt-authz');
 
-// defining the Express app
-const app = express();
-
-// List of available Pizza (By default)
-//TODO: could be a MongoDB...
-
-const pizzas = [
-    {pizza: 'Pizza Marinara', description: 'Sliced mozzarella, basil, and extra virgin olive oil.', price:'10'},
-    {pizza: 'Sicilian Pizza', description: 'Features tomatoes, basil, and extra virgin olive oil.', price:'12'},
-    {pizza: 'Greek Pizza', description: 'Features tomatoes, sliced mozzarella, and extra virgin olive oil.', price:'15'}
-  ];
-//List of pizza orders.
-const orders = [];
-
+const cors = require('cors');
 require('dotenv').config();
+
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
   throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
 }
 
+const corsOptions =  {
+  origin: 'http://localhost:3000'
+};
+// enabling CORS for all requests
+app.use(cors(corsOptions));
+
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 // adding Helmet to enhance your API's security
 app.use(helmet());
-
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
 
+
+const pizzas = [
+  {pizza: 'Pizza Marinara', description: 'Sliced mozzarella, basil, and extra virgin olive oil.', price:'10'},
+  {pizza: 'Sicilian Pizza', description: 'Features tomatoes, basil, and extra virgin olive oil.', price:'12'},
+  {pizza: 'Greek Pizza', description: 'Features tomatoes, sliced mozzarella, and extra virgin olive oil.', price:'15'}
+];
+//List of pizza orders.
+const orders = [];
+
 const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: 'https://gaanva.auth0.com/.well-known/jwks.json'
-    }),
-  
-    // Validate the audience and the issuer.
-    audience: process.env.AUTH0_AUDIENCE,
-    issuer: 'https://gaanva.auth0.com/',
-    algorithms: ['RS256']
-  });
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://gaanva.auth0.com/.well-known/jwks.json'
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: 'https://gaanva.auth0.com/',
+  algorithms: ['RS256']
+});
 //setting authorization levels
-const checkScopesUser = jwtAuthz(['read:pizza','read:myOrders']);
-const checkScopesAdmin = jwtAuthz(['read:pizza','create:pizza', 'read:orders', 'read:users']);
+const checkScopesUser = jwtAuthz(['read:pizza','read:myOrders'],{checkAllScopes: true});
+const checkScopesAdmin = jwtAuthz(['read:pizza','create:pizza', 'read:orders', 'read:users'], {checkAllScopes: true});
 
 // My public request... return all available pizzas
 app.get('/', (req, res) => {
@@ -66,7 +64,8 @@ app.get('/', (req, res) => {
 });
 //consulting orders made.
 app.get('/orders', checkJwt, checkScopesAdmin, (req, res) => {
-    return res.status(200).json(this.orders);
+  console.log(orders);  
+  return res.status(200).json(orders);
 });
 
 app.get('/myOrders', checkJwt, checkScopesUser, (req, res) => {
@@ -74,9 +73,12 @@ app.get('/myOrders', checkJwt, checkScopesUser, (req, res) => {
   var email = req.query.user_email
   var userOrders = [];
   //search for user orders
-  for(order in this.orders){
-    if(order.user_email==email){
-      userOrders.push(order);
+  console.log(orders);
+  var i=0;
+  for(i=0; i<orders.length; i++){
+    if(orders[i].user_mail===email){
+      console.log('order user email match!');
+      userOrders.push(orders[i]);
     }
   }
   return res.status(200).json(userOrders);
@@ -90,15 +92,14 @@ app.post('/pizza', checkJwt, checkScopesAdmin, (req, res)=>{
 
 //customer request for a pizza order
 app.post('/order', checkJwt, checkScopesUser, (req, res) => {
-  orders.push(req.body);
+  console.log('oder recieved: ');
+  console.log(req.body.order);
+  orders.push(req.body.order);
   return res.status(201).json('Order successfully created!');
 });
 
 //TODO: deleting pizza order...
 //TODO: updating pizza order...  
-
-
-
 
 // starting the server
 app.listen(3001, () => {
